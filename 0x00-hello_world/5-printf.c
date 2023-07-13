094de3233f18536c7c65cdd0709ca7200a4e2f56
Hlulibooysen <hlulelwabooysen09@gmail.com>
2023-07-13 16:51:39 -0700

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/**
- * main - Entry 
- * Return: always 0 (success)
- */
-int main(void)
-{
-        printf("with proper grammer, but the outcome is a piece of art,\n");
-        return (0);
