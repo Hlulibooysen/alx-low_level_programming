225aeeed53915ae3f47cc447551daf9e914b3e6d
Hlulibooysen <hlulelwabooysen09@gmail.com>
2023-07-17 13:46:05 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+/**
+ * main - A c program that prints a line to the standard error 
+ * Return 1 (Success)
+ */
+int main(void) 
+{
+fput("and that place of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
+return (1);
+}
