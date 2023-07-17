6b3f988d2856f12feb289f2aee71820e08c01f83
Hlulibooysen <hlulelwabooysen09@gmail.com>
2023-07-17 16:05:45 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point; 
+ * Description: 'print alphabelts lowercase'
+ * Return: Always (0); 
+ */
+int main(void)
+{
+puntchar(65);
+puntchar('\n')
+return (0);
+}
